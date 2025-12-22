0679256555d1e3e9058aac8c0ebda7cab83d5a40
gferreira-valcann <gferreira@valcann.com.br>
2025-12-22 13:58:25 -0300

    changes in script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
+nomeApp="lab4-app"
+cd /appdir
+npm install
 
-set -e
-
-APP_DIR=/home/ubuntu/lab4
-APP_NAME=lab4-app
-
-cd $APP_DIR
-
-# install dependencies as ubuntu user
-sudo -u ubuntu npm install --production
-
-# reload supervisor configs
-sudo supervisorctl reread
-sudo supervisorctl update
-
-# restart app safely
-if sudo supervisorctl status | grep -q "$APP_NAME.*RUNNING"; then
-  sudo supervisorctl restart $APP_NAME
-else
-  sudo supervisorctl start $APP_NAME
-fi
-
-
-#nomeApp="lab4-app"
-#cd ../
-#npm install
-
-#sudo supervisord
-#sudo supervisorctl start $nomeApp
+sudo supervisord
+sudo supervisorctl start $nomeApp
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#nomeApp="lab4-app"
+nomeApp="lab4-app"
 
-#sudo supervisorctl stop $nomeApp
-set +e
-
-supervisorctl stop lab4-app || true
-
-exit 0
+sudo supervisorctl stop $nomeApp
